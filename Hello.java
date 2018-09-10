59da2b37830d68cb85092fe204d79d73a11f7680
Logan <logan717@gmail.com>
2018-09-10 15:29:30 -0500

    Initial Commit

---
+++ Hello.java
@@ -0,0 +1,11 @@
+//First program in Java
+/*
+    Hello.Java
+*/
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.exit(0);
+    }
+}
